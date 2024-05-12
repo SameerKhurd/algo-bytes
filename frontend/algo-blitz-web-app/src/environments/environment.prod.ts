30fdba0fe9091abd91b21a2df4eb3bfa3d7fe6d8
Sameer Khurd <sameerkhurd@gmail.com>
2024-05-12 15:55:38 +0530

    Configured frontend prodcution endpoints

--- frontend/algo-blitz-web-app/src/environments/environment.prod.ts
+++ frontend/algo-blitz-web-app/src/environments/environment.prod.ts
@@ -1,12 +1,9 @@
 export const environment = {
   production: true,
-
-  applicationUrl:
-    'http://127.0.0.1:5001/fire-playground-100/asia-south1/question',
-  questionUrl: 'http://127.0.0.1:5001/fire-playground-100/asia-south1/question',
-  submissionUrl:
-    'http://127.0.0.1:5001/fire-playground-100/asia-south1/submission',
+  applicationUrl: 'https://application-5pcvasnbuq-el.a.run.app',
+  questionUrl: 'https://question-5pcvasnbuq-el.a.run.app',
+  submissionUrl: 'https://submission-5pcvasnbuq-el.a.run.app',
   userQuestionSubmissionsUrl:
-    'http://127.0.0.1:5001/fire-playground-100/asia-south1/submission',
-  bookmarkUrl: 'http://127.0.0.1:5001/fire-playground-100/asia-south1/bookmark',
+    'https://userquestionsubmissions-5pcvasnbuq-el.a.run.app',
+  bookmarkUrl: 'https://bookmark-5pcvasnbuq-el.a.run.app',
 };
